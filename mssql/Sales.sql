470b05ae3b7d4fc5e179dd7d05da1f34ed2a5115
Fernando Calmet <fercalmet@gmail.com>
2020-10-16 20:01:28 -0500

    Create Sales database script

---
+++ mssql/Sales.sql
@@ -0,0 +1,259 @@
+USE [Sales]
+GO
+/****** Object:  Table [dbo].[Account] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Account](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[status] [varchar](30) NOT NULL,
+	[createdDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Address] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Address](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[accountId] [int] NOT NULL,
+	[street] [nvarchar](100) NOT NULL,
+	[city] [varchar](30) NOT NULL,
+	[state] [varchar](30) NOT NULL,
+	[country] [varchar](30) NOT NULL,
+	[zipcode] [varchar](10) NOT NULL,
+	[createdDate] [datetime] NOT NULL,
+	[lastUpdateDate] [datetime] NULL,
+ CONSTRAINT [PK_Address] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[CreditCard] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[CreditCard](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[accountId] [int] NOT NULL,
+	[nameOnCard] [varchar](50) NOT NULL,
+	[number] [varchar](100) NOT NULL,
+	[type] [varchar](50) NOT NULL,
+	[expirationDate] [date] NOT NULL,
+	[createdDate] [datetime] NOT NULL,
+	[lastUpdateDate] [datetime] NULL,
+ CONSTRAINT [PK_CreditCard] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Customer] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Customer](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[accountId] [int] NOT NULL,
+	[firstName] [varchar](30) NOT NULL,
+	[lastName] [varchar](30) NOT NULL,
+	[email] [nvarchar](50) NOT NULL,
+	[createdDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [IX_Customer] UNIQUE NONCLUSTERED 
+(
+	[email] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Order] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Order](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[accountId] [int] NOT NULL,
+	[shipmentId] [int] NOT NULL,
+	[status] [varchar](30) NOT NULL,
+	[totalAmount] [int] NOT NULL,
+	[totalTax] [decimal](18, 2) NULL,
+	[createdDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[OrderDetails] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderDetails](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[orderId] [int] NOT NULL,
+	[productId] [int] NOT NULL,
+	[quantity] [int] NOT NULL,
+	[price] [money] NOT NULL,
+	[tax] [decimal](18, 2) NULL,
+ CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Payment] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Payment](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[orderId] [int] NOT NULL,
+	[method] [varchar](50) NOT NULL,
+	[totalAmount] [money] NOT NULL,
+	[transactionDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Payment] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Product] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Product](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+	[description] [text] NULL,
+	[sku] [varchar](12) NULL,
+	[size] [decimal](18, 2) NULL,
+	[upc] [varchar](12) NOT NULL,
+	[unitPrice] [money] NOT NULL,
+	[unitInStock] [int] NULL,
+	[createdDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Shipment] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Shipment](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[addressId] [int] NOT NULL,
+	[trackingId] [varchar](50) NOT NULL,
+	[status] [varchar](30) NOT NULL,
+	[provider] [nvarchar](100) NOT NULL,
+	[price] [money] NOT NULL,
+ CONSTRAINT [PK_Shipment] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+ALTER TABLE [dbo].[Address]  WITH CHECK ADD  CONSTRAINT [FK_Address_Account] FOREIGN KEY([accountId])
+REFERENCES [dbo].[Account] ([id])
+GO
+ALTER TABLE [dbo].[Address] CHECK CONSTRAINT [FK_Address_Account]
+GO
+ALTER TABLE [dbo].[CreditCard]  WITH CHECK ADD  CONSTRAINT [FK_CreditCard_Account] FOREIGN KEY([accountId])
+REFERENCES [dbo].[Account] ([id])
+GO
+ALTER TABLE [dbo].[CreditCard] CHECK CONSTRAINT [FK_CreditCard_Account]
+GO
+ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_Account] FOREIGN KEY([accountId])
+REFERENCES [dbo].[Account] ([id])
+GO
+ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_Account]
+GO
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_Order_Account] FOREIGN KEY([accountId])
+REFERENCES [dbo].[Account] ([id])
+GO
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_Order_Account]
+GO
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_Order_Shipment] FOREIGN KEY([shipmentId])
+REFERENCES [dbo].[Shipment] ([id])
+GO
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_Order_Shipment]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Order] FOREIGN KEY([orderId])
+REFERENCES [dbo].[Order] ([id])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Order]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Product] FOREIGN KEY([productId])
+REFERENCES [dbo].[Product] ([id])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Product]
+GO
+ALTER TABLE [dbo].[Payment]  WITH CHECK ADD  CONSTRAINT [FK_Payment_Order] FOREIGN KEY([orderId])
+REFERENCES [dbo].[Order] ([id])
+GO
+ALTER TABLE [dbo].[Payment] CHECK CONSTRAINT [FK_Payment_Order]
+GO
+ALTER TABLE [dbo].[Shipment]  WITH CHECK ADD  CONSTRAINT [FK_Shipment_Address] FOREIGN KEY([addressId])
+REFERENCES [dbo].[Address] ([id])
+GO
+ALTER TABLE [dbo].[Shipment] CHECK CONSTRAINT [FK_Shipment_Address]
+GO
